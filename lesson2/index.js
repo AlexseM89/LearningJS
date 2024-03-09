28bd9f7d22ae2132f9495ccfc659ff441a368740
Aleksei Melenchuk <melenchuk89@mail.ru>
2024-03-09 12:15:22 +0300

    Добавлен файл второго занятия

--- lesson2/index.js
+++ lesson2/index.js
@@ -1,1 +1,50 @@
 //second coomit
+//Строковое преобразование
+// const testStringVar = String(45);
+//Преобразование к числу
+//const testNumberVar = Number("      ");
+// const testNumberVar = +undefined;
+//Boolean преобразования
+// const testBooleanVar = !!"0";
+
+//console.log(testBooleanVar, typeof testBooleanVar);
+//Математические операторы
+// console.log(2 + 5);
+// console.log(2 * 5);
+// console.log(2 - 5);
+// console.log(10 / 5);
+// console.log(10 % 2);
+// console.log(11 % 2);
+// console.log(2 ** 3);
+
+//Унарный плюс
+
+// console.log(false + "Sergei");
+//Оператор сравнения
+// console.log(1 == true);
+//сравнени строк
+// console.log("авто" == "авто");
+// console.log("Ap" > "aps");
+// console.log("авто" == "авто ");
+// console.log(null == undefined);
+// console.log(NaN == NaN);
+//Строгое сравнение
+console.log(5 == "5");
+console.log(5 === "5");
+console.log(null == undefined);
+console.log(null === undefined);
+console.log(Number.isNaN(NaN));
+console.log(5 !== 5);
+console.log(5 != 5);
+console.log(5 != "5");
+console.log(5 != "1");
+//Сравнения объектов
+const obj1 = {};
+const obj2 = {};
+//console.log( === obj2);
+//console.log("2" > "12");
+console.log(undefined == null);
+console.log(typeof ("" - 1));
+console.log(7 / 0);
+console.log(typeof (" -9 " + 5));
+console.log(undefined + 1);
